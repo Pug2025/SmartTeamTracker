98ce6d48c5991a63b04fc9634730638fc7f2d257
Pug2025 <7x829mqjzv@privaterelay.appleid.com>
2026-03-06 09:30:30 -0500

    Reduce setup date field weight

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /* ===== App Version ===== */
-const APP_VERSION = '6.2.19';
+const APP_VERSION = '6.2.20';
 
 const IS_LOCAL_DEV_HOST = ['localhost', '127.0.0.1'].includes(window.location.hostname);
 const IS_SPECTATOR_MODE = !!window.__spectatorMode;

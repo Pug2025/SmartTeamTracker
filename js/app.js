8e1522d959701e3edfc1e8702152eda6a1fbcbd1
Pug2025 <7x829mqjzv@privaterelay.appleid.com>
2026-03-06 09:16:24 -0500

    Normalize setup date input

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /* ===== App Version ===== */
-const APP_VERSION = '6.2.16';
+const APP_VERSION = '6.2.17';
 
 const IS_LOCAL_DEV_HOST = ['localhost', '127.0.0.1'].includes(window.location.hostname);
 const IS_SPECTATOR_MODE = !!window.__spectatorMode;
